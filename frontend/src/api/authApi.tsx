142401cb38c56bfb5d5988c4b7a340482fdf6659
marianoconchillo <marianoconchillo@hotmail.com>
2023-03-08 19:24:55 -0300

    npm start command

--- frontend/src/api/authApi.tsx
+++ frontend/src/api/authApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authApi = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://authentication-app-production-88e8.up.railway.app/api",
 });
 
 export default authApi;
